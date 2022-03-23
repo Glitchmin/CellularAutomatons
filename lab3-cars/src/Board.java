3f6702f76dd8c9aa6ad29be1179e27add7314fd8
Adam <adam.p.kulczycki@gmail.com>
2022-03-23 11:13:32 +0100

    Cars added

---
+++ lab3-cars/src/Board.java
@@ -0,0 +1,147 @@
+import javax.swing.*;
+import javax.swing.event.MouseInputListener;
+import java.awt.*;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.MouseEvent;
+
+public class Board extends JComponent implements MouseInputListener, ComponentListener {
+    private static final long serialVersionUID = 1L;
+    private Point[][] points;
+    private int size = 10;
+    public int editType = 0;
+
+    public Board(int length, int height) {
+        addMouseListener(this);
+        addComponentListener(this);
+        addMouseMotionListener(this);
+        setBackground(Color.WHITE);
+        setOpaque(true);
+    }
+
+    private void initialize(int length, int height) {
+        points = new Point[length][height];
+
+        for (int x = 0; x < points.length; ++x) {
+            for (int y = 0; y < points[x].length; ++y) {
+                points[x][y] = new Point();
+            }
+        }
+        // TODO
+    }
+
+    public void iteration() {
+
+        // TODO
+
+        for (int x = 0; x < points.length; ++x) {
+            for (int y = 0; y < points[x].length; ++y) {
+                // TODO
+            }
+        }
+        this.repaint();
+    }
+
+    public void clear() {
+        for (int x = 0; x < points.length; ++x)
+            for (int y = 0; y < points[x].length; ++y) {
+                points[x][y].clear();
+            }
+        this.repaint();
+    }
+
+
+    protected void paintComponent(Graphics g) {
+        if (isOpaque()) {
+            g.setColor(getBackground());
+            g.fillRect(0, 0, this.getWidth(), this.getHeight());
+        }
+        g.setColor(Color.GRAY);
+        drawNetting(g, size);
+    }
+
+    private void drawNetting(Graphics g, int gridSpace) {
+        Insets insets = getInsets();
+        int firstX = insets.left;
+        int firstY = insets.top;
+        int lastX = this.getWidth() - insets.right;
+        int lastY = this.getHeight() - insets.bottom;
+
+        int x = firstX;
+        while (x < lastX) {
+            g.drawLine(x, firstY, x, lastY);
+            x += gridSpace;
+        }
+
+        int y = firstY;
+        while (y < lastY) {
+            g.drawLine(firstX, y, lastX, y);
+            y += gridSpace;
+        }
+
+        for (x = 0; x < points.length; ++x) {
+            for (y = 0; y < points[x].length; ++y) {
+                float a = 1.0F;
+
+                // TODO
+                //g.setColor(new Color(R, G, B, 0.7f));
+
+                g.fillRect((x * size) + 1, (y * size) + 1, (size - 1), (size - 1));
+            }
+        }
+
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        int x = e.getX() / size;
+        int y = e.getY() / size;
+        if ((x < points.length) && (x > 0) && (y < points[x].length) && (y > 0)) {
+            if (editType == 0) {
+                points[x][y].clicked();
+            }
+            this.repaint();
+        }
+    }
+
+    public void componentResized(ComponentEvent e) {
+        int dlugosc = (this.getWidth() / size) + 1;
+        int wysokosc = (this.getHeight() / size) + 1;
+        initialize(dlugosc, wysokosc);
+    }
+
+    public void mouseDragged(MouseEvent e) {
+        int x = e.getX() / size;
+        int y = e.getY() / size;
+        if ((x < points.length) && (x > 0) && (y < points[x].length) && (y > 0)) {
+            if (editType == 0) {
+                points[x][y].clicked();
+            }
+            this.repaint();
+        }
+    }
+
+    public void mouseExited(MouseEvent e) {
+    }
+
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void componentShown(ComponentEvent e) {
+    }
+
+    public void componentMoved(ComponentEvent e) {
+    }
+
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    public void mouseMoved(MouseEvent e) {
+    }
+
+    public void componentHidden(ComponentEvent e) {
+    }
+
+    public void mousePressed(MouseEvent e) {
+    }
+
+}
---
+++ lab3-cars/src/Point.java
@@ -0,0 +1,17 @@
+public class Point {
+
+    // TODO
+
+    public void move() {
+        // TODO
+    }
+
+    public void clicked() {
+        // TODO
+    }
+
+    public void clear() {
+        // TODO
+    }
+}
+
